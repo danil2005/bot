71f5640886916883cc4081437b6001ad52c009d3
PotapenkovDanil <d.potapenkov@mirtekgroup.ru>
2024-11-29 16:35:09 +0300

    Скелет проекта

---
+++ handlers/user_handlers.py
@@ -0,0 +1,9 @@
+from aiogram import F, Router
+from aiogram.filters import Command, CommandStart
+from aiogram.types import CallbackQuery, Message
+from lexicon.lexicon import LEXICON
+
+router = Router()
+
+
+
