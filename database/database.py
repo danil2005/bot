10ef51ead53b821900103277577031698d715915
DESKTOP-V81V673\Danil <potapenkov.danil@mail.ru>
2024-12-01 19:58:28 +0300

    промежуточный коммит

--- database/database.py
+++ database/database.py
@@ -1,1 +1,7 @@
+import sqlite3
 
+def add_questionnaire(data: dict):
+    with sqlite3.connect("example.db") as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users")
+        rows = cursor.fetchall()
