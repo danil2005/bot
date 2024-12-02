973c1452cde026f3eb332fa461a098b0bb1e3dd8
DESKTOP-V81V673\Danil <potapenkov.danil@mail.ru>
2024-12-02 22:15:24 +0300

    Добавлен файл

---
+++ services/services.py
@@ -0,0 +1,14 @@
+
+def create_questionnaire_text (data:dict) -> str:
+    return (
+        "Вот ваша анкета:\n\n"
+        f"Имя - {data['name']}\n"
+        f"Возраст - {data['old']}\n"
+        f"Пол - {data['gender']}\n"
+        f"Рост - {data['height']}\n"
+        f"Вес - {data['weight']}\n"
+        "\nВсе верно?"
+    )
+
+
+
