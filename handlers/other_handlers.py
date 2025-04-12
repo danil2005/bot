c381f5515de027f8e08abb8e4c81849cf203bb2e
Potapenkov_Home_PC <potapenkov.danil@mail.ru>
2025-04-12 11:14:18 +0300

    Добавлены строки документации для модуля workout_handlers

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -5,5 +5,5 @@ router = Router()
 
 @router.message()
 async def other_messages(message: Message):
-    """Команда отвечает сообщения, которые не попвли не в один обработчик"""
+    """Команда отвечает сообщения, которые не попали не в один обработчик"""
     await message.answer(f"Что-то пошло не так(\nВведите команду /start")
